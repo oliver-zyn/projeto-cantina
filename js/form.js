848575d75a088e71b1c9286058d1c86d02ba5e35
Oliverzyn <olivermayer15@gmail.com>
2022-03-08 15:50:50 -0300

    refatoração funções validação email / senha

--- js/form.js
+++ js/form.js
@@ -1,18 +1,24 @@
 
-let simbolosEmail = {
-    emailErro: document.querySelector("#erroEmail"),
-    emailOk: document.querySelector("#okEmail"),
-    msgEmail: document.querySelector("#msgErroEmail")
+let oSimbolosEmail = {
+    Erro: document.querySelector("#erroEmail"),
+    Ok: document.querySelector("#okEmail"),
+    msg: document.querySelector("#msgErroEmail")
 }
 
-let simbolosSenha = {
-    senhaErro: document.querySelector("#erroSenha"),
-    senhaOk: document.querySelector("#okSenha"),
-    msgSenha: document.querySelector("#msgErroSenha")
+let oSimbolosSenha = {
+    Erro: document.querySelector("#erroSenha"),
+    Ok: document.querySelector("#okSenha"),
+    msg: document.querySelector("#msgErroSenha")
 }
 
-function validaEmail(email) {
-    let valorEmail = email.value
+let oSimbolosConfirmaSenha = {
+    Erro: document.querySelector("#erroConfirmSenha"),
+    Ok: document.querySelector("#okConfirmSenha"),
+    msg: document.querySelector("#msgErroConfirmSenha")
+}
+
+function validaEmail(tagEmail, bModalEsqueciSenha) {
+    let valorEmail = tagEmail.value
 
     if (valorEmail.indexOf(' ') >= 0) {
         mostraErro(0, 'O email não pode conter espaços!')
@@ -20,79 +26,62 @@ function validaEmail(email) {
         let validacaoRegex = /\S+@\S+\.\S+/;
         let resultadoValidacao = validacaoRegex.test(valorEmail);
 
-        if (resultadoValidacao == true) {
-            mostraOk(0, email)
+        if (!bModalEsqueciSenha) {
+            if (resultadoValidacao) {
+                mostraOk(oSimbolosEmail, tagEmail)
+            } else {
+                mostraErro(oSimbolosEmail, tagEmail, 'Email Inválido!')
+            }
         } else {
-            mostraErro(0, email,'Email Inválido!')        
+            if (resultadoValidacao == true) {
+                alert('Email enviado com sucesso!')
+            } else {
+                alert('Email Inválido!')
+            }
         }
     }
 }
 
-function validaSenha(senha) {
-    let valorSenha = senha.value
+function validaSenha(tagSenha) {
+    let valorSenha = tagSenha.value
 
     if (valorSenha.indexOf(' ') >= 0) {
-        mostraErro(1, senha, 'A senha não pode conter espaços!')
-    } else if (valorSenha.lenght < 8){
-        mostraErro(1, senha, 'A senha deve ter no mínimo 8 dígitos!')
+        mostraErro(oSimbolosSenha, tagSenha, 'A senha não pode conter espaços!')
+    } else if (valorSenha.length < 8) {
+        mostraErro(oSimbolosSenha, tagSenha, 'A senha deve ter no mínimo 8 dígitos!')
     } else {
-        mostraOk(1, senha)
+        mostraOk(oSimbolosSenha, tagSenha)
     }
 }
 
-function mostraErro(tipoCampo, input, msgErro) {
-    if (tipoCampo == 0) {
-      resetaSimbolosValidacaoEmail()
-      simbolosEmail.emailErro.style.display = "block";
-      simbolosEmail.msgEmail.style.display = "block";
-      simbolosEmail.msgEmail.innerHTML = msgErro;
-      input.className = 'form-control inputErro'
-    } else if (tipoCampo == 1) {
-      resetaSimbolosValidacaoSenha()
-      simbolosSenha.senhaErro.style.display = "block";
-      simbolosSenha.msgSenha.style.display = "block";
-      simbolosSenha.msgSenha.innerHTML = msgErro;
-      input.className = 'form-control inputErro'
-    }
-  }
-  
-  function mostraOk(campo, input) {
-      if (campo == 0) {
-          resetaSimbolosValidacaoEmail()
-          simbolosEmail.emailOk.style.display = "block";
-          input.className = 'form-control inputOk'
-      } else if (campo == 1) {
-          resetaSimbolosValidacaoSenha()
-          simbolosSenha.senhaOk.style.display = "block";
-          input.className = 'form-control inputOk'
-      }
-  }
-  
-  function resetaSimbolosValidacaoEmail() {
-      for (let item in simbolosEmail) {
-        simbolosEmail[item].style.display = 'none'
-      }
-  }
-  
-  function resetaSimbolosValidacaoSenha() {
-      for(let item in simbolosSenha) {
-          simbolosSenha[item].style.display = 'none'
-      }
-  }
 
-  function validaEmailEsqueciASenha() {
-    let valorEmail = document.querySelector('#inputEmailEsqueciASenha').value
+function validaSenhaIgual(tagConfirmaSenha, sIdCampoSenha) {
+    let valorConfirmaSenha = tagConfirmaSenha.value
+    let valorSenha = document.querySelector(`#${sIdCampoSenha}`).value
 
-    if (valorEmail.indexOf(' ') >= 0) {
-        alert('O email não pode conter espaços!')
+    if (valorConfirmaSenha !== valorSenha) {
+        mostraErro(oSimbolosConfirmaSenha, tagConfirmaSenha, 'As senhas devem ser iguais!')
     } else {
-        let validacaoRegex = /\S+@\S+\.\S+/;
-        let resultadoValidacao = validacaoRegex.test(valorEmail);
+        mostraOk(oSimbolosConfirmaSenha, tagConfirmaSenha)
+    }
+}
 
-        if (resultadoValidacao == true) {
-            alert('Email enviado com sucesso!')
-        } else {
-            alert('Email Inválido!')        
-        }
+function mostraErro(objSimbolos, tagInput, msgErro) {
+    resetaSimbolosValidacao(objSimbolos);
+    objSimbolos.Erro.style.display = "block";
+    objSimbolos.msg.style.display = "block";
+    objSimbolos.msg.innerHTML = msgErro;
+    tagInput.className = 'form-control inputErro';
+}
+
+function mostraOk(objSimbolos, tagInput) {
+    resetaSimbolosValidacao(objSimbolos);
+    objSimbolos.Ok.style.display = "block";
+    tagInput.className = 'form-control inputOk';
+}
+
+function resetaSimbolosValidacao(objSimbolos) {
+    for (let item in objSimbolos) {
+        objSimbolos[item].style.display = 'none';
     }
 }
