3968cae01f5cfe280de8a8958f5b61eb9426b572
Oliverzyn <olivermayer15@gmail.com>
2022-03-31 20:27:40 -0300

    commit projeto main

---
+++ js/carrinho.js
@@ -0,0 +1,131 @@
+let pedidos = []
+
+//#region Tarefas Iniciais
+
+function carregaPedidos() {
+    let pedidosStorage = JSON.parse(localStorage.getItem('pedidosCarrinho'))
+
+    if (!pedidosStorage) {
+        return;
+    } else {
+        for(let item of pedidosStorage) {
+            pedidos.push(item)
+        }
+        atualizaCarrinho()
+    }
+}
+
+//#endregion
+
+//#region Eventos
+
+function addCarrinho(id, nome, preco, imagem) {
+
+    let {valorContador} = dadosContador(id)
+
+    let idProduto = 'b' + id
+
+    for(let i = 0; i < valorContador; i++) {
+        let pedidoExiste = buscaProduto(idProduto)
+
+        if (!pedidoExiste) {
+            pedidos.push({idProduto, nome, preco, imagem, quantidade: valorContador})
+            atualizaCarrinho()
+            break
+        } else {
+            atualizaContador(idProduto)
+            break
+        }
+    }
+}
+
+
+function buscaProduto(idProduto) {
+    let produto = pedidos.find(item => item.idProduto == idProduto)
+
+    return produto
+}
+
+function atualizaCarrinho() {
+    let tagPedidos = document.querySelector('#pedidos');
+    tagPedidos.innerHTML = ""
+
+    pedidos.forEach(pedido => {
+        tagPedidos.innerHTML += `
+                                <div class="pedido">
+                                <div class="quantidade pedido--quantidade">
+                                    <button onclick="decrementClick('${pedido.idProduto}', true)">-</button>
+                                    <p id="${pedido.idProduto}" class="num-contador-pedido" data-contador=${pedido.quantidade}>${pedido.quantidade}</p>
+                                    <button onclick="incrementClick('${pedido.idProduto}', true)">+</button>
+                                </div>
+                                    <div class="pedido--item">
+                                        <div class="pedido--img">
+                                            <img src=${pedido.imagem} alt=${pedido.nome}>
+                                        </div>
+                                        <div class="pedido--texto">
+                                            <h2>${pedido.nome}</h2>
+                                            <p>R$${pedido.preco.toFixed(2)}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                `
+    })
+
+    atualizaNotificacao()
+    atualizaSubTotal()
+}
+
+function atualizaContador(idProduto) {
+    let {valorContador} = dadosContador(idProduto.replace(/^./, ""))
+    let {tagContador} = dadosContador(idProduto)
+
+    atualizaQtdItensCarrinho(idProduto, tagContador, valorContador)
+    
+    atualizaSubTotal()
+
+}
+
+function atualizaQtdItensCarrinho(idProduto, tagContador, valorContador) {
+    let indexPedidos = pedidos.findIndex(pedido => {
+        return pedido.idProduto === idProduto
+    })
+
+    pedidos[indexPedidos].quantidade = valorContador
+
+    updateDisplay(tagContador, pedidos[indexPedidos].quantidade)
+}
+
+function removeCarrinho(idProduto) {
+    for (let indice in pedidos) {
+        let pedido = pedidos[indice];
+        if (pedido.idProduto == idProduto) {
+            pedidos.splice(indice, 1)
+
+            atualizaCarrinho()
+        }
+    }
+}
+
+function atualizaSubTotal() {
+    let tagSubTotal = document.querySelector('#subtotal')
+    let subtotal = 0
+
+    pedidos.forEach(pedido => {
+        subtotal += pedido.preco * pedido.quantidade
+    })
+
+    tagSubTotal.innerHTML = `<strong>Subtotal:</strong> R$ ${subtotal}`
+    gravaPedidos()
+}
+
+function atualizaNotificacao() {
+    let notf = document.querySelector('#notificacao')
+
+    notf.innerHTML = pedidos.length
+}
+
+function gravaPedidos() {
+    localStorage.setItem('pedidosCarrinho', JSON.stringify(pedidos))
+}
+
+//#endregion
---
+++ js/form.js
@@ -0,0 +1,100 @@
+
+let oSimbolosEmail = {
+    Erro: document.querySelector("#erroEmail"),
+    Ok: document.querySelector("#okEmail"),
+    msg: document.querySelector("#msgErroEmail")
+}
+
+let oSimbolosSenha = {
+    Erro: document.querySelector("#erroSenha"),
+    Ok: document.querySelector("#okSenha"),
+    msg: document.querySelector("#msgErroSenha")
+}
+
+let oSimbolosConfirmaSenha = {
+    Erro: document.querySelector("#erroConfirmSenha"),
+    Ok: document.querySelector("#okConfirmSenha"),
+    msg: document.querySelector("#msgErroConfirmSenha")
+}
+
+function validaEmail(tagEmail, bModalEsqueciSenha) {
+    let tagEmailF;
+    let valorEmail;
+
+    if (bModalEsqueciSenha) {
+        tagEmailF = document.querySelector('#inputEmailEsqueciASenha')
+        valorEmail = tagEmailF.value
+    } else {
+        tagEmailF = tagEmail
+        valorEmail = tagEmail.value
+    }
+
+    if (valorEmail == '') {
+        return mostraErro(oSimbolosEmail, tagEmailF, 'Não deixe o campo em branco!')
+    } else {
+        if (valorEmail.indexOf(' ') >= 0) {
+            mostraErro(oSimbolosEmail, tagEmailF, 'O email não pode conter espaços!')
+        } else {
+            let validacaoRegex = /\S+@\S+\.\S+/;
+            let resultadoValidacao = validacaoRegex.test(valorEmail);
+
+            if (!bModalEsqueciSenha) {
+                if (resultadoValidacao) {
+                    mostraOk(oSimbolosEmail, tagEmail)
+                } else {
+                    mostraErro(oSimbolosEmail, tagEmail, 'Email Inválido!')
+                }
+            } else {
+                if (resultadoValidacao) {
+                    alert('Email enviado com sucesso!')
+                } else {
+                    alert('Email Inválido!')
+                }
+            }
+        }
+    }
+}
+
+function validaSenha(tagSenha) {
+    let valorSenha = tagSenha.value
+
+    if (valorSenha.indexOf(' ') >= 0) {
+        mostraErro(oSimbolosSenha, tagSenha, 'A senha não pode conter espaços!')
+    } else if (valorSenha.length < 8) {
+        mostraErro(oSimbolosSenha, tagSenha, 'A senha deve ter no mínimo 8 dígitos!')
+    } else {
+        mostraOk(oSimbolosSenha, tagSenha)
+    }
+}
+
+
+function validaSenhaIgual(tagConfirmaSenha, sIdCampoSenha) {
+    let valorConfirmaSenha = tagConfirmaSenha.value
+    let valorSenha = document.querySelector(`#${sIdCampoSenha}`).value
+
+    if (valorConfirmaSenha !== valorSenha) {
+        mostraErro(oSimbolosConfirmaSenha, tagConfirmaSenha, 'As senhas devem ser iguais!')
+    } else {
+        mostraOk(oSimbolosConfirmaSenha, tagConfirmaSenha)
+    }
+}
+
+function mostraErro(objSimbolos, tagInput, msgErro) {
+    resetaSimbolosValidacao(objSimbolos);
+    objSimbolos.Erro.style.display = "block";
+    objSimbolos.msg.style.display = "block";
+    objSimbolos.msg.innerHTML = msgErro;
+    tagInput.className = 'form-control inputErro';
+}
+
+function mostraOk(objSimbolos, tagInput) {
+    resetaSimbolosValidacao(objSimbolos);
+    objSimbolos.Ok.style.display = "block";
+    tagInput.className = 'form-control inputOk';
+}
+
+function resetaSimbolosValidacao(objSimbolos) {
+    for (let item in objSimbolos) {
+        objSimbolos[item].style.display = 'none';
+    }
+}
